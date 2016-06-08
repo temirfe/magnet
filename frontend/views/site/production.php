0d81b87952b09f7480084222e86b010c25857e31
Temir Asus <temirbek@gmail.com>
2016-06-08 13:00:36 +0600

    wassap

---
+++ frontend/views/site/production.php
@@ -0,0 +1,28 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'Magnet Production';
+?>
+<style>
+    body{
+        overflow-x: hidden;
+        background: url(/css/images/about.jpg) no-repeat center center fixed;
+        -webkit-background-size: cover;
+        -moz-background-size: cover;
+        -o-background-size: cover;
+        background-size: cover;
+    }
+</style>
+<div class="site-about mtop text-center roboto text-uppercase font18 lh30 white">
+
+    Food Processor Marta MT-1549 {мощность 700Вт, 4 аксессуара: измельчитель с чашей 500 мл, насадка для измельчения/смешивания, венчик для взбивания, мерный стакан, 8 скоростных режимов, турборежим, подсветка кнопок, цвет белый)Food Processor Marta MT-1549 {мощность 700Вт, 4 аксессуара: измельчитель с чашей 500 мл, насадка для измельчения/смешивания, венчик для взбивания, мерный стакан, 8 скоростных режимов, турборежим, подсветка кнопок, цвет белый)
+
+    9 223 372 036 854 775 807
+
+    LED TV 55" Samsung UE55H6500ATXUZ Black, 1920x1080,400 Гц, 20 Вт, 4 HDMI, 3 USB, Режим футбол, DVB-T2, Smart Tv, Wi-Fi, 3D, 3D-очки ( 2 ), поворотная подставка, пульт Smart Control, сателлит
+    AVENT Детские вилочка, ложечка и нож 18мес+ 12140
+    aventДетскиевилочка,ложечкаинож18мес+ 12139
+</div>
